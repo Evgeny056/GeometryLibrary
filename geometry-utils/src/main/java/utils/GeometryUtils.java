620bfd65bce57f1e4cdf97a20098fea0e53b802e
Evgeny Shurupov <schuevgeny@yandex.ru>
2024-05-20 20:25:03 +0300

    Added area comparison method to GeometryUtils class. changed version to 1.1.0-SNAPSHOT

--- geometry-utils/src/main/java/utils/GeometryUtils.java
+++ geometry-utils/src/main/java/utils/GeometryUtils.java
@@ -4,4 +4,8 @@ public class GeometryUtils {
     public static double convertToSquareMeters(double inSquareFeet) {
         return inSquareFeet * 0.092903;
     }
+
+    public static int compareSquares(double area1, double area2) {
+        return Double.compare(area1, area2);
+    }
 }
